8708b77ae0ee6f4d35dc949235a6f247628ae211
Aruzhan Narymbayeva <aruzhan@Aruzhans-MacBook-Pro.local>
2023-11-12 17:35:06 +0600

    use debounce

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,83 @@
+import React, { useCallback, useRef, useState } from "react";
+import { SearchContext } from "../../App";
+import debounce from "lodash.debounce";
+import styles from "./Search.module.scss";
+
+const Search = () => {
+  const [value, setValue] = useState();
+  const { setSearchValue } = React.useContext(SearchContext);
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setSearchValue("");
+    setValue("");
+    inputRef.current.focus();
+  };
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+      // console.log(str);
+    }, 250),
+    []
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
+
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title />
+        <g data-name="Layer 2" id="Layer_2">
+          <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
+        </g>
+      </svg>
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+      {value && (
+        <svg
+          onClick={() => onClickClear()}
+          className={styles.clearIcon}
+          version="1.1"
+          viewBox="0 0 14 14"
+        >
+          <title />
+          <desc />
+          <defs />
+          <g
+            fill="none"
+            fillRule="evenodd"
+            id="Page-1"
+            stroke="none"
+            strokeWidth="1"
+          >
+            <g
+              fill="#000000"
+              id="Core"
+              transform="translate(-341.000000, -89.000000)"
+            >
+              <g id="close" transform="translate(341.000000, 89.000000)">
+                <path
+                  d="M14,1.4 L12.6,0 L7,5.6 L1.4,0 L0,1.4 L5.6,7 L0,12.6 L1.4,14 L7,8.4 L12.6,14 L14,12.6 L8.4,7 L14,1.4 Z"
+                  id="Shape"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+      )}
+    </div>
+  );
+};
+export default Search;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Category from "../components/Category";
+import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PIzzaBlock/PizzaBlock";
+import Skeleton from "../components/PIzzaBlock/Skeleton";
+import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
+
+const Home = () => {
+  const categoryId = useSelector((state) => state.filter.categoryId);
+  const sortType = useSelector((state) => state.filter.sort.sortProperty);
+  const currentPage = useSelector((state) => state.filter.currentPage);
+  const dispatch = useDispatch();
+
+  const { searchValue } = React.useContext(SearchContext);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // const [currentPage, setCurrentPage] = useState(1);
+
+  const onChangeCategory = (id) => {
+    console.log(id);
+    dispatch(setCategoryId(id));
+  };
+  const onChangePage = (number) => {
+    dispatch(setCurrentPage(number));
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    const sortBy = sortType?.replace("-", "");
+    const order = sortType?.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? `category=&{categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
+
+    axios
+      .get(
+        `https://632d38db0d7928c7d246d6e6.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((res) => {
+        setItems(res.data);
+
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
+  const pizzas = items.map((object) => (
+    <PizzaBlock key={object.id} {...object} />
+  ));
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Category value={categoryId} onChangeCategory={onChangeCategory} />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+    </div>
+  );
+};
+
+export default Home;
